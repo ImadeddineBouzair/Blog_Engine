3d33a89a24d3654831c1fce74d6f6cd23ace2350
ImadeddineBouzair <bouzairimad@gmail.com>
2024-05-18 17:27:33 -0700

    scuring the application

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
 
@@ -8,7 +14,27 @@ const articleRoutes = require('./routes/articleRoutes');
 
 const app = express();
 
-app.use(express.json());
+// Set security HTTP headers
+app.use(helmet());
+
+// Limiting the requests
+const limiter = rateLimit({
+  max: 50,
+  windowMs: 60 * 60 * 1000,
+  message: 'To many requests, Pleas try again later!',
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '30kb' }));
+
+// MongoSanitize need to use it after express.json()
+app.use(mongoSanitize());
+
+// Protection against "Cross site scripting" attacks
+app.use(xss());
+
+// Preventing paramiter pollution
+app.use(hpp());
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/articles', articleRoutes);
